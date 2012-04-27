b7bf868939c719facddaa7944c5fd6fd404369d8
parnaudo-satellite <parnaudo@gmail.com>
2012-04-27 14:20:38 -0700

    test

--- test2.php
+++ test2.php
@@ -1,24 +1,47 @@
 <?php 
-function multidimensional_search($parents, $searched) { 
-  if (empty($searched) || empty($parents)) { 
-    return false; 
-  } 
-  
-  foreach ($parents as $key => $value) { 
-    $exists = true; 
-    foreach ($searched as $skey => $svalue) { 
-      $exists = ($exists && IsSet($parents[$key][$skey]) && $parents[$key][$skey] == $svalue); 
-    } 
-    if($exists){ return $key; } 
-  } 
-  
-  return false; 
-} 
+include("lib/init.php");
+$dataminer=new dataMiner;
+$queryDoctors = "select * from topneurologistsnetworkmeasures where atomId IN (1699766238)";
+echo $queryDoctors;
+//$queryDoctors = "select * from neurologist where paperCount>10 and paperCountFullAuthor>6 order by paperCount Desc";
+$result = mysql_query($queryDoctors) or die(mysql_error());
+while($row=mysql_fetch_array($result)){
+  	$query=array();
+	$count=0;
+	$filer="[AUTHOR]";
+	$author=$row['authorName']; //your query term, searches for both middle name and middle initials	
+	echo $author."<BR>";
+	$query[]=$author;
+	$query[]=$filter;
+	$uids=$dataminer->eSearch($query,0);
+		foreach($uids['papers'] as $key=>$paperID){
+		  $paperInfo=$dataminer->eFetch($paperID);
+		  foreach($paperInfo['authors'] as $field){
+				$authorPosition=0;
+			  foreach($field->children() as $authors){
 
-$parents = array(); 
-$parents[] = array(1320883200, 3); 
-$parents[] = array(1320883200, 5); 
-$parents[] = array(1318204800, 5); 
+				  $authorPosition++;
+				  $pubmedName=$authors->LastName." ".$authors->Initials;
+				  $lastName=$authors->LastName;
+				  $foreName=$authors->ForeName;
+				  if(stripos($author,$pubmedName)===0){
+	
+					 if($authorPosition==1){
+					 	$updateQuery="UPDATE topneurologistsnetworkmeasures set location='".mysql_escape_string($paperInfo['affiliation'])."' where atomId='".$row['atomId']."'";
+						$updateQuery=$updateQuery;
+						mysql_query($updateQuery) or die ("Error in query: $query. ".mysql_error());
+						exit;	  	
+					 }
+					}		
+		/*		  
+			 if($paperInfo['affiliation']!=''){
+			 	$updateQuery="UPDATE topneurologistsnetworkmeasures set location='".$paperInfo['affiliation']."' where atomId='".$row['atomId']."'";
+			 echo $updateQuery."<BR>";
+		 	}*/
+			}
+		 }
+	}
+//if there are papers, insert an author record	
 
-echo multidimensional_search($parents, array(1318204800, 5)); // 1 
+}
 ?>
