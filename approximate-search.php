9b07dba0f0cfadaea1ad1fdc70f88b76fdeef3e0
Paul Arnaudo <parnaudo@gmail.com>
2015-02-06 13:03:16 -0800

    Update approximate-search.php

--- approximate-search.php
+++ approximate-search.php
@@ -1,7 +1,7 @@
 <?php
 /*******************************************************************************
  * Copyright (C) 2003-2006 by Jarno Elonen <elonen@iki.fi>
- *
+ * W
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1,
